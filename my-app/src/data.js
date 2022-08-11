0d58314745a4f5a7f4124acd51afc0f5136307f9
Kazune Shidara <kazuneshidara@gmail.com>
2022-08-10 22:43:39 -0700

    finalizing styling

--- my-app/src/data.js
+++ my-app/src/data.js
@@ -7,7 +7,7 @@ export default [
         link: "link1",
         startDate: "January 3rd",
         endDate: "January 4th",
-        description: "description1",
+        description: "Arguably one of my favorite places to go back to every year. I'm not sure if it's because Grandma is there and is always super welcoming or if it's because it's such a prime location away from central Tokyo. Residential, clean, accessible, and voted the most liveable place in all of Japan for 2019.",
         image: "kawaguchi.png",
         pin:"locationPin.png"
     },
@@ -19,7 +19,7 @@ export default [
         link: "link2",
         startDate: "June 29",
         endDate: "June 29",
-        description: "description2",
+        description: "A 40 minute train ride north east of Central Milan, this hidden, opulent, and relatively unknown place was an amazing destination. The lake was obviously the main attraction but all the surrounding shops, restaurants, and businesses also didn't take away from the impressiveness of the town.",
         image: "como.png",
         pin:"locationPin.png"
 
@@ -32,7 +32,7 @@ export default [
         link: "link3",
         startDate: "May 21",
         endDate: "June 12",
-        description: "description3",
+        description: "A go-to place to visit for me every year. It's such a different place from Northern California, it's hard to believe that it's part of the same state. Always sunny and an interesting melting pot of people with various jobs and personalities. Excited to explore it this year.",
         image: "oc.png",
         pin:"locationPin.png"
 
@@ -42,4 +42,3 @@ export default [
 ]
 
 
-//description: "A 40 minute train ride north east of Central Milan, this hidden, opulent, and relatively unknown place was an amazing destination. The lake was obviously the main attraction but all the surrounding shops, restaurants, and businesses also didn't take away from the impressiveness of the town. ",
