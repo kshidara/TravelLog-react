4fcd0a6dafa126fd80c4a53fe6928c245196d1ae
Kazune Shidara <kazuneshidara@gmail.com>
2022-08-10 22:31:38 -0700

    more css stylinf

--- my-app/src/Card.js
+++ my-app/src/Card.js
@@ -11,7 +11,7 @@ export default function Card(props) {
             <section className="card--rest">
                 <img className="pin--img" src={props.pin} />
                 <span className="card--location">{props.location}</span>
-                <a href="{props.link}">View on Google Maps</a>
+                <a className="card--link" href="{props.link}">View on Google Maps</a>
                 <h2 className="card--title">{props.title}</h2>
                 <span className="card--dates"> {props.startDate} - {props.endDate}</span>
                 <p className="card--description">{props.description}</p>
